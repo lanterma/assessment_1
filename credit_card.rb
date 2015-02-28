1f3ea28aeb8ef54acfad2891a9bcfe9803388413
Jason Noble <github+jasonn@jasonnoble.org>
2015-02-27 20:01:52 -0700

    Added CreditCard class file

---
+++ credit_card.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# This is a credit card class
+class CreditCard
+  attr_reader :balance
+  def intialize
+    @balance = 0.00
+  end
+
+  def charge(amount)
+    @balance += amount
+  end
+end
